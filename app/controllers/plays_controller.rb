7e482c18d9e3a89614763e4880c18cdf15f79a61
Murad Sarker <sarker24@gmail.com>
2020-11-14 15:49:15 +0100

    Add the show and index action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,5 +1,11 @@
 class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
   def index
+    @plays = Play.all.order("created_at DESC")
+  end
+
+  def show
+    
   end
 
   def new
@@ -7,7 +13,10 @@ class PlaysController < ApplicationController
    end
 
   def create
-    @play.new(play_params)
+    @play= Play.new(play_params)  
+    if @play.save
+      redirect_to root_path
+    end
   end
 
   def edit
@@ -24,5 +33,10 @@ class PlaysController < ApplicationController
   def play_params
     params.require(:play).permit(:title, :description, :director)
   end
+
+  def find_play
+    @play = Play.find(params[:id])
+  end
+
 
 end
